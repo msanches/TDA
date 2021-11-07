96fca226a6515b6de013c1d9dfc9ded494852009
Marco Antonio Sanches <marcosanches.dev@gmail.com>
2021-11-07 14:47:39 -0300

    Aulas 11 e 12

---
+++ Aula 11/Exemplo.py
@@ -0,0 +1,9 @@
+def altera(a):
+    a = a+2
+    print(a)
+
+a = 3
+altera(a)
+print(a)
+print(a)
+print(a)
---
+++ Aula 11/ParametroPorReferencia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ParametroPorReferencia {
+    public static void main(String[] args) {
+        int[] num = new int[3];
+        Scanner sc = new Scanner(System.in);
+        //Leitura e armazenamento do vetor
+        for(int i=0; i<3; i++){
+            System.out.print("Digite o "+(i+1)+"º número: ");
+            num[i] = sc.nextInt();
+        }
+        multiplica(num);
+        System.out.println("\nValores após a chamada do método");
+        for(int i=0; i<3; i++)
+            System.out.println("Numero: " + num[i]);
+    }
+
+    public static void multiplica(int num[]){
+        for(int i=0; i<3; i++)
+            num[i] = num[i] * 2;
+    }
+}
